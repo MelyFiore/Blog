e156344902d85c27eb2d5f8e63beb63319d4caf5
MelyFiore <melifiore.edu@gmail.com>
2022-05-11 21:55:33 -0500

    Usé custom fields

--- single.php
+++ single.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
 
 	<section class="container detalles">
-			<?php the_post_thumbnail("large"); ?>
+			
 			<h1><?php the_title(); ?></h1>
+			<p class="resumen">Resumen: <?php the_field('resumen'); ?></p>
+			<?php the_post_thumbnail("large"); ?>
 			<?php the_content(); ?>
+			<h4>Fuente: <?php the_field('fuente'); ?></h4>
+			
+
 	</section>
 	
 	<?php get_footer(); ?>
